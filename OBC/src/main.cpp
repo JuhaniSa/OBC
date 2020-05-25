19dd86f97dcf6ae6ebb5cc09a8271fe043d9f1f9
kbjusa <juhani.sankari@tuni.fi>
2020-05-26 01:51:39 +0300

    Main.cpp changed

--- OBC/src/main.cpp
+++ OBC/src/main.cpp
@@ -1,30 +1,63 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <menu.h>
+byte data_lenght [77];
+byte byte_number;
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,16,2);
+unsigned int AFR_RAW; //AFR value
+float o2val; //AFR value
+int MAP=-1; //MAP from speeduino
+int BARO; //barometer from speeduino
+float boost1;
+float boost;
 
-int button_pin = 2;
-
-LiquidCrystal_I2C lcd (0x27,16,2);
-Menu.
-
-void setup() {
-  lcd.init();
-  lcd.begin(16,2);
-  lcd.backlight();
-  lcd.print("ok");
-  pinMode(button_pin,INPUT);
-  delay(2000);
+void req_data_r() //Requests all the data available.
+{
+Serial.write("r");
+Serial.write(0x00); //Speeduino TS canID, not used atm.
+Serial.write(0x30); //command type, 0x30 for real time data.
+Serial.write(4); //offset for the data. LSB
+Serial.write(0x00); // offset is in 2 bytes. LSB first.
+Serial.write(37); //how many bytes we need back. LSB
+Serial.write(0x00); // number of bytes is in 2 bytes. LSB first.
 }
 
-void loop() 
+void read_data()
 {
-  lcd.clear();
-  if (digitalRead(button_pin) == LOW){
-    Menu.
-  lcd.print("Pressed");
-  delay(50);
-  };
 
-  // put your main code here, to run repeatedly:
-};
+}
+void setup() {
+  lcd.backlight();
+  lcd.init();
+  lcd.begin(16,2);
+  lcd.print("Lcd-intialized");
+  Serial.begin(115200);
+  delay(2000);
+  lcd.clear();
+  req_data_r();
+  
+}
+
+void loop()
+ {
+  if (Serial.available() > 0)
+  {
+   lcd.clear();
+   int info_ = Serial.read();
+   Serial.print(info_);
+
+  }
+  
+  else
+  {
+    lcd.clear();
+    lcd.print("kakkaa");
+    delay(2000);
+  }
+  delay(50);
+  
+
+
+
+  
+}
