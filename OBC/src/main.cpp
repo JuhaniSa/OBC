e835af1b7c889937cf9921456d40c1e713b70555
kbjusa <juhani.sankari@tuni.fi>
2020-05-29 00:32:25 +0300

    Data transfer works both ways!!

--- OBC/src/main.cpp
+++ OBC/src/main.cpp
@@ -4,6 +4,9 @@
 byte data_bytes [39];
 byte ResponseLength =37;
 byte byte_number;
+byte out_input [4];
+byte send [7];
+byte send_data_bytes[8];
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27,16,2);
 int TPS;
 int ADV;
@@ -11,6 +14,8 @@ int VE;
 int AE;
 int IAT;
 int MAP;
+
+
 
 void req_data_r() //Requests all the data available.
 {
@@ -23,7 +28,7 @@ Serial.write(ResponseLength); //how many bytes we need back. LSB
 Serial.write(0x00); // number of bytes is in 2 bytes. LSB first.
 }
 
-void read_data(byte data [])
+void read_data(byte data [])  //Saves data from temporary array (and prints it for debug..)
 {
   TPS=(data[25]);
   ADV=data[24];
@@ -48,19 +53,23 @@ void setup() {
   Serial.begin(115200);//Start serial
   delay(2000);
   lcd.clear();
-  req_data_r();//Send request for data
-  
+}
+void send_data()
+{
+  char start = Serial.read();
 }
 
-void loop()
- {
+
+
+void save_data()
+{
   if (Serial.available() > 0)//Checks if serial data is recieved
   {
     for (int b=0;b<sizeof(data_bytes);b++)//Read and save all the data to Byte array
     {
       data_bytes[b]=Serial.read();
       }
-    delay(5);
+    
     /*
     for (int i ;i<sizeof(data_bytes);i++)   //Print all data from byte array to serial
     {
@@ -79,24 +88,58 @@ void loop()
     read_data(data_bytes);   //Calls function to process data
   }
 
-   delay(10);
-  
   }
-  
   
   else
   {
     lcd.clear();
     lcd.print("No connection!");
-   
     delay(2000);
     lcd.clear();
-    Serial.begin(115200);
     
   }
   memset(data_bytes,0,sizeof(data_bytes));
   byte_number=0;
   req_data_r();
   
-  
 }
+
+
+void loop()
+ {
+   lcd.clear();
+   int sensor0 = analogRead(A0);
+   
+   if (Serial.available()>0)
+   {
+     for(int i=0;i<sizeof(out_input);i++){
+       out_input[i] = Serial.read();
+       Serial.print(out_input[i]);
+     }
+     if (out_input[0]==82){
+       Serial.write("G");
+       Serial.write(1);//Confirms valid data request
+       Serial.write(0);//Input chanel
+
+       Serial.write(sensor0);//Data bit
+       Serial.write(0);
+       Serial.write(0);
+       Serial.write(0);
+       Serial.write(0);
+       Serial.write(0);
+       Serial.write(0);
+       Serial.write(0);
+      
+
+     }
+     
+     }
+     delay(50);
+     save_data();
+     delay(50);
+   }
+  //save_data();
+
+ 
+ 
+  
